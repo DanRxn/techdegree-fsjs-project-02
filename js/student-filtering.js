9c2c8a2885f9b37c1d40224379a0e5cbf6620386
Dan Robertson <dan@productintuition.com>
2018-01-18 09:18:25 -0800

    Project setup
    ✅ Create new JS file for my program
    ✅ Link new JS file to index.html
    ✅ Link to external jQuery file in index.html
    
    Also added some comments about to-dos 👌

---
+++ js/student-filtering.js
@@ -0,0 +1,30 @@
+// # Dynamic page content and student-filtering functionality
+
+	// ## Validate that external JS has loaded
+
+		// ### Project's JS
+
+		console.log('student-filtering.js has loaded!');
+
+		// ### jQuery from CDN
+
+		const justTesting = $('h2');
+
+		if (justTesting) {
+			console.log('jQuery has loaded from CDN too!')
+		}
+
+
+
+// 	## Pagination
+
+
+	//TO-DOs
+
+	// ◻️ Configure results per page (as a constant)
+
+
+	// ◻️ Select list of students and set as constant
+
+
+	// ◻️ Calculate number of pages needed, based on number of students, and set as constant
