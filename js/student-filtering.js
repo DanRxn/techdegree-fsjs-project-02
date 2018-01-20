f3b64e879c8622eca7e637ef1ab57e2e48a4c1b8
Dan Robertson <dan@productintuition.com>
2018-01-19 18:15:59 -0800

    Some JS added
    ✅ Configure results per page (as a constant)
    
    ✅ Select list of students and set as constant
    
    ✅ Calculate number of pages needed, based on number of students, and set as constant

--- js/student-filtering.js
+++ js/student-filtering.js
@@ -21,10 +21,64 @@
 
 	//TO-DOs
 
-	// ◻️ Configure results per page (as a constant)
+	// ✅ Configure results per page (as a constant)
+
+const resultsPerPage = 10;
 
 
-	// ◻️ Select list of students and set as constant
-
+	// ✅ Select list of students and set as constant
+const students = $('.student-item');
 
 	// ◻️ Calculate number of pages needed, based on number of students, and set as constant
+
+const getNumberOfPages = () => {
+	const countOfStudents = $(students).length;
+
+	numberOfPages = Math.ceil( countOfStudents / resultsPerPage );
+	return numberOfPages;
+	
+}
+
+	// On page load
+
+		// ◻️ Show links for the correct number of pages
+
+$('.page').append(
+`<!-- pagination HTML to create dynamically -->
+      <div class="pagination">
+        <ul>
+        </ul>
+      </div>
+      <!-- end pagination -->`
+	);
+
+// Below isn't working.  I *think* it has to do with the getNumberOfPages() function not being used right. 
+
+// for ( p = 1, p <= getNumberOfPages(), p++) {
+// 	$('.pagination ul').append(
+// 	  `<li>
+//             <a href="#">` + p + `</a>
+//           </li>`
+// 	);
+// }
+
+		// ◻️ Hide all but the first page of results; and
+
+
+		// ◻️ Style the link for first page as the current page
+
+
+
+
+// 	## Search
+
+
+	//TO-DOs
+
+	// 
+
+	
+	// 
+
+
+	// 
